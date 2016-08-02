3af0eded471fe574cda1d1738273212384592279
Shawn Inder <shawn@vigour.io>
2016-08-02 15:26:47 -0400

    remove need for babel

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const test = require('tape')
-const hash = require('../')
+var test = require('tape')
+var hash = require('../')
 
-const testCases = [
+var testCases = [
   'ASDFLKJH56789)*&^$`Ω≈ç√¥¨∆†',
   123456789
 ]
@@ -11,9 +11,9 @@ const testCases = [
 test('hash', (t) => {
   t.plan(2 * testCases.length)
   testCases.forEach((item) => {
-    const hsh = hash(item)
-    const len = hsh.length
-    const hsh2 = hash(item)
+    var hsh = hash(item)
+    var len = hsh.length
+    var hsh2 = hash(item)
     t.equals(len > 4 && len < 8, true, 'hash(' + item + ').length.length > 4 && < 8')
     t.equals(hsh, hsh2, 'should always produce the same hash')
   })
